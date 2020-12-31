8824cb8b4220cda15316e576ee4cf1d16e70979e
alexliusq <uilxela7@gmail.com>
2020-12-31 23:20:46 +0800

    add modulus method

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -7,8 +7,12 @@ def reverse_with_string(x: int) -> int:
     return -reversed_int if x < 0 else reversed_int
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    absolute_x = abs(x)
+    reversed_int = 0
+    while(absolute_x > 0):
+        reversed_int = reversed_int * 10 + (absolute_x % 10)
+        absolute_x //= 10
+    return -reversed_int if x < 0 else reversed_int
 
 
 if __name__ == '__main__':
@@ -16,3 +20,7 @@ if __name__ == '__main__':
     generic_test.generic_test_main('reverse_digits.py',
                                     'reverse_digits.tsv',
                                     reverse_with_string)
+    print('modulus method')
+    generic_test.generic_test_main('reverse_digits.py',
+                                    'reverse_digits.tsv',
+                                    reverse)
