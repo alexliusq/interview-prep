ae01450a1dd7b044ff0fc984643ef68f419bac65
alexliusq <uilxela7@gmail.com>
2020-12-31 11:54:39 +0800

    solve 4.1 parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,10 +1,28 @@
 from test_framework import generic_test
 
 
-def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
 
+def parity(x: int) -> int:
+    result = 0
+    while (x):
+        result ^= 1
+        x = x & (x - 1)
+    return result
+
+PRECOMPUTED_PARITY = [parity(x) for x in range(2**16)]
+
+def parity_precomputed(x: int) -> int:
+    BIT_MASK = 0xFFFF
+    # BIT_MASK = 2^16 - 1
+    result = 0
+    while(x):
+        result ^= PRECOMPUTED_PARITY[x & BIT_MASK]
+        x >>= 16
+    return result
 
 if __name__ == '__main__':
-    exit(generic_test.generic_test_main('parity.py', 'parity.tsv', parity))
+    generic_test.generic_test_main('parity.py', 'parity.tsv', parity)
+    print('Pre-computed parity')
+    generic_test.generic_test_main('parity.py', 'parity.tsv', parity_precomputed)
+
+
