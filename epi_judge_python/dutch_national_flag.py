a7749961e733d912b5e768220673f8cb4499e71a
alexliusq <uilxela7@gmail.com>
2021-01-01 23:30:00 +0800

    EPI 2 pass method

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -8,10 +8,41 @@ from test_framework.test_utils import enable_executor_hook
 RED, WHITE, BLUE = range(3)
 
 
-def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
-    # TODO - you fill in here.
-    return
+# def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+#     center_value = A[pivot_index]
+#     next_pivot = 0
+#     num_equals = 0
+#     next_greater = len(A) - 1
+#     while (next_pivot <= next_greater):
+#         # print(f'p{next_pivot} e{num_equals} g{next_greater}')
+#         if (A[next_pivot] < center_value):
+#             if (num_equals > 0):
+#                 A[next_pivot], A[next_pivot - num_equals] = A[next_pivot - num_equals], A[next_pivot]
+#             next_pivot += 1
+#         elif (A[next_pivot] == center_value):
+#             next_pivot += 1
+#             num_equals += 1
+#         else:
+#             A[next_pivot], A[next_greater] = A[next_greater], A[next_pivot]
+#             next_greater -= 1
+#     return A
 
+# two pass method
+def dutch_flag_partition(pivot_index: int, A: List[int]) -> None:
+    pivot = A[pivot_index]
+    smaller = -1
+    larger = len(A)
+    for i in range(len(A)):
+        if (A[i] < pivot):
+            smaller += 1
+            A[smaller], A[i] = A[i], A[smaller]
+    for i in reversed(range(len(A))):
+        if (A[i] < pivot):
+            break
+        if (A[i] > pivot):
+            larger -= 1
+            A[larger], A[i] = A[i], A[larger]
+    return A
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
