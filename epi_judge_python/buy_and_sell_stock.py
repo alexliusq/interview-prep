6f5bd4febcf076263adc4efb0728eb8fb65b2151
alexliusq <uilxela7@gmail.com>
2021-01-02 00:03:14 +0800

    solution to buy sell stock once

--- epi_judge_python/buy_and_sell_stock.py
+++ epi_judge_python/buy_and_sell_stock.py
@@ -2,14 +2,31 @@ from typing import List
 
 from test_framework import generic_test
 
+## should be correct, passes everything but crashes at test 400
+# def brute_force_stock(prices: List[float]) -> float:
+#     # print(len(prices))
+#     differences = [prices[j] - prices[i]
+#                    for i in range(len(prices)) for j in range(i, len(prices))]
+#     print(len(differences))
+#     max_profit = max(differences)
+#     return max_profit if max_profit > 0 else 0
 
 def buy_and_sell_stock_once(prices: List[float]) -> float:
-    # TODO - you fill in here.
-    return 0.0
-
+    lowest_price = float('inf')
+    max_profit = 0.0
+    for i in range(len(prices)):
+        if (prices[i] < lowest_price):
+            lowest_price = prices[i]
+        profit = prices[i] - lowest_price
+        if (profit > max_profit):
+            max_profit = profit
+    return max_profit
 
 if __name__ == '__main__':
-    exit(
-        generic_test.generic_test_main('buy_and_sell_stock.py',
-                                       'buy_and_sell_stock.tsv',
-                                       buy_and_sell_stock_once))
+    # print('buy sell brute force')
+    # generic_test.generic_test_main('buy_and_sell_stock.py',
+    #                                 'buy_and_sell_stock.tsv',
+    #                                 brute_force_stock)
+    generic_test.generic_test_main('buy_and_sell_stock.py',
+                                    'buy_and_sell_stock.tsv',
+                                    buy_and_sell_stock_once)
