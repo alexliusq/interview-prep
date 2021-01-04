f972f84aede89337eaf4f4da176056908b39dd13
alexliusq <uilxela7@gmail.com>
2021-01-04 11:34:47 +0800

    optimized solution

--- epi_judge_python/prime_sieve.py
+++ epi_judge_python/prime_sieve.py
@@ -7,6 +7,7 @@ from test_framework import generic_test
 ## checks 1, sqrt(2), sqrt(3)... sqrt(n)
 ## if it was 1,...,n, then run time is n(n+1)/2
 ## but since it's all square root... then maybe n(sqrt(n) + 1 )/2??
+## definitely O(n * sqrt(n))
 def brute_force(n: int) -> List[int]:
     primes = []
     for i in range(2, n+1):
@@ -22,14 +23,20 @@ def brute_force(n: int) -> List[int]:
 
 # Given n, return all primes up to and including n.
 def generate_primes(n: int) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    ## generate boolean array, O(n)
+    primes = []
+    possible_prime = [True for x in range(0, n+1)]
+    for i in range(2, n+1):
+        if possible_prime[i]:
+            primes.append(i)
+            for j in range(i, n+1, i):
+                possible_prime[j] = False
+    return primes
 
 
 if __name__ == '__main__':
-    print('brute force')
-    generic_test.generic_test_main('prime_sieve.py', 'prime_sieve.tsv',
-                                       brute_force)
-    # exit(
-    #     generic_test.generic_test_main('prime_sieve.py', 'prime_sieve.tsv',
-    #                                    generate_primes))
+    # print('brute force')
+    # generic_test.generic_test_main('prime_sieve.py', 'prime_sieve.tsv',
+    #                                    brute_force)
+        generic_test.generic_test_main('prime_sieve.py', 'prime_sieve.tsv',
+                                       generate_primes)
