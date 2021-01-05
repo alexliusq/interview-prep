4f00b876bccd655e38b65141c0c6ce216770c3eb
alexliusq <uilxela7@gmail.com>
2021-01-05 21:46:55 +0800

    O(n) time, O(1) space solution

--- epi_judge_python/reverse_words.py
+++ epi_judge_python/reverse_words.py
@@ -6,17 +6,37 @@ from test_framework.test_utils import enable_executor_hook
 
 # Assume s is a list of strings, each of which is of length 1, e.g.,
 # ['r', 'a', 'm', ' ', 'i', 's', ' ', 'c', 'o', 's', 't', 'l', 'y'].
-def reverse_words(s):
-    # TODO - you fill in here.
-    return
 
+def reverse_words(s) -> None:
+    def reverse_range(s: list, start: int, end: int):
+        while start < end:
+            s[start], s[end] = s[end], s[start]
+            start += 1
+            end -= 1
+    reverse_range(s, 0, len(s) - 1)
+    word_end = 0
+    word_start = 0
+    while word_end < len(s):
+        if s[word_end] == ' ':
+            if word_end > word_start:
+                reverse_range(s, word_start, word_end - 1)
+            word_end += 1
+            word_start = word_end
+        else:
+            word_end += 1
+    ## reverse the last word
+    reverse_range(s, word_start, len(s) - 1)
+
+        
+def pythonic_reverse_words(s):
+    return ' '.join(reversed(s.split(' ')))
 
 @enable_executor_hook
 def reverse_words_wrapper(executor, s):
     s_copy = list(s)
 
     executor.run(functools.partial(reverse_words, s_copy))
-
+    # print(id(s_copy))
     return ''.join(s_copy)
 
 
