a4692f9c655655b45a6827693fbf8768e2453e28
alexliusq <uilxela7@gmail.com>
2021-01-05 15:05:34 +0800

    O(n) solution for replace and remove

--- epi_judge_python/replace_and_remove.py
+++ epi_judge_python/replace_and_remove.py
@@ -6,8 +6,23 @@ from test_framework.test_utils import enable_executor_hook
 
 
 def replace_and_remove(size: int, s: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    result_size = 0
+    for i in range(size):
+        if s[i] != 'b':
+            s[result_size] = s[i]
+            result_size += 1
+    count_a = s[:result_size].count('a')
+    full_size = result_size + count_a
+    idx = result_size - 1
+    while count_a > 0:
+        if s[idx] != 'a':
+            s[idx + count_a] = s[idx]
+        else:
+            s[idx + count_a] = 'd'
+            count_a -= 1
+            s[idx + count_a] = 'd'
+        idx -= 1
+    return full_size
 
 
 @enable_executor_hook
