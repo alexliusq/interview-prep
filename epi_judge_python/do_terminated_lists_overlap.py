f04aab1a5db994ea11842da99ace420a607d3a0c
alexliusq <uilxela7@gmail.com>
2021-01-07 21:32:40 +0800

    O(a) space brute force solution

--- epi_judge_python/do_terminated_lists_overlap.py
+++ epi_judge_python/do_terminated_lists_overlap.py
@@ -1,4 +1,5 @@
 import functools
+from typing import Optional
 
 from list_node import ListNode
 from test_framework import generic_test
@@ -6,9 +7,18 @@ from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
-def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
+def overlapping_no_cycle_lists(l0: ListNode, l1: ListNode) -> Optional[ListNode]:
+    l0_nodes = set()
+    head0 = ListNode(0, l0)
+    head1 = ListNode(0, l1)
+    while head0.next:
+        head0 = head0.next
+        l0_nodes.add(id(head0))
+    while head1.next:
+        if id(head1) in l0_nodes:
+            return head1
+        head1 = head1.next
+    return None
 
 
 @enable_executor_hook
@@ -32,7 +42,7 @@ def overlapping_no_cycle_lists_wrapper(executor, l0, l1, common):
 
     result = executor.run(functools.partial(overlapping_no_cycle_lists, l0,
                                             l1))
-
+    # print(common)
     if result != common:
         raise TestFailure('Invalid result')
 
