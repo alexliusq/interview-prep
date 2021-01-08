c93c56b7a69ab2a016f11500392c199d4f4e488d
alexliusq <uilxela7@gmail.com>
2021-01-08 14:47:32 +0800

    O(n) solution

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,29 @@
+from typing import List
 from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+    i = 0
+    stack = []
+    operations = {
+        '*': lambda x, y: x * y,
+        '/': lambda x, y: x //y,
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x -y
+    }
+    parsed = expression.split(',')
+
+    while i < len(parsed):
+        element = parsed[i]
+        i += 1
+        if element in operations:
+            y = int(stack.pop())
+            x = int(stack.pop())
+            result = operations[element](x, y)
+            stack.append(result)
+        else:
+            stack.append(int(element))
+    return stack[-1]
 
 
 if __name__ == '__main__':
