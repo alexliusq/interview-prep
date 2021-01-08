88933f288d6de0aee8136b819adf504be8e178fd
alexliusq <uilxela7@gmail.com>
2021-01-08 20:32:17 +0800

    the very pythonic way of doing it

--- epi_judge_python/tree_level_order.py
+++ epi_judge_python/tree_level_order.py
@@ -5,29 +5,44 @@ from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+# def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
+#     queue = deque()
+#     queue.append(tree)
+#     result = []
+#     if not tree:
+#         return result
+#     while queue:
+#         next_level: List[BinaryTreeNode] = []
+#         key_level: List[int] = []
+#         while queue:
+#             node = queue.popleft()
+#             next_level.append(node)
+#             key_level.append(node.data)
+#         if key_level:
+#             result.append(key_level)
+#         for node in next_level:
+#             if node.left:
+#                 queue.append(node.left)
+#             if node.right:
+#                 queue.append(node.right)
+            
+            
+#     return result
+
 def binary_tree_depth_order(tree: BinaryTreeNode) -> List[List[int]]:
-    queue = deque()
-    queue.append(tree)
-    depth_order = []
-    while queue:
-        next_level: List[BinaryTreeNode] = []
-        key_level: List[int] = []
-        while queue:
-            node = queue.popleft()
-            if not node:
-                continue
-            next_level.append(node)
-            key_level.append(node.data)
-        if key_level:
-            depth_order.append(key_level)
-        for node in next_level:
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            
-            
-    return depth_order
+    result = []
+    curr_depth: List[BinaryTreeNode] = []
+    curr_depth.append(tree)
+    if not tree:
+        return result
+    while curr_depth:
+        result.append([curr.data for curr in curr_depth])
+        curr_depth = [
+            child for curr in curr_depth for child in (curr.left, curr.right)
+             if child
+        ]
+    
+    return result
 
 
 if __name__ == '__main__':
