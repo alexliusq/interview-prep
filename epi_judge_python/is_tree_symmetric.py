98645661d809ba79bdd7bdaad2e6c1d10480a849
alexliusq <uilxela7@gmail.com>
2021-01-09 19:57:36 +0800

    optimal solution in first try

--- epi_judge_python/is_tree_symmetric.py
+++ epi_judge_python/is_tree_symmetric.py
@@ -3,8 +3,19 @@ from test_framework import generic_test
 
 
 def is_symmetric(tree: BinaryTreeNode) -> bool:
-    # TODO - you fill in here.
-    return True
+    def check_symmetry(left, right) -> bool:
+        # print(left, right)
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left.data != right.data:
+            return False
+        return (check_symmetry(left.left, right.right)
+            and check_symmetry(left.right, right.left))
+    if not tree:
+        return True
+    return check_symmetry(tree.left, tree.right)
 
 
 if __name__ == '__main__':
