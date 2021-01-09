7434e9257a6c3cf0f97f7a2528d24c0862566ea8
alexliusq <uilxela7@gmail.com>
2021-01-10 00:04:14 +0800

    cleaner version

--- epi_judge_python/tree_with_parent_inorder.py
+++ epi_judge_python/tree_with_parent_inorder.py
@@ -5,8 +5,46 @@ from test_framework import generic_test
 
 
 def inorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    prev = None
+    result = []
+    # print(tree)
+    while tree:
+        if prev is tree.parent:
+            ## came down from prev
+            if tree.left:
+                next = tree.left
+                # prev = tree
+                # tree = tree.left
+            else:
+                result.append(tree.data)
+                next = tree.right or tree.parent
+                ## cleaner version above, with or short-circuiting
+                # if tree.right:
+                #     prev = tree
+                #     tree = tree.right
+                # else:
+                #     prev = tree
+                #     tree = tree.parent
+        # if prev is tree.left:
+        ## need to make sure after each case we re-loop, don't re-examine
+        ## or else you might run into None errors
+        elif prev is tree.left:
+            result.append(tree.data)
+            next = tree.right or tree.parent
+            # if tree.right:
+            #     prev = tree
+            #     tree = tree.right
+            # else:
+            #     prev = tree
+            #     tree = tree.parent
+        # if prev is tree.right:
+        else:
+            next = tree.parent
+            # prev = tree
+            # tree = tree.parent
+        
+        prev, tree = tree, next
+    return result
 
 
 if __name__ == '__main__':
