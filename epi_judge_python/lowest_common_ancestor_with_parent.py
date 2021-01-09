f1628dcfd64b8427ae741af33596e242856b4211
alexliusq <uilxela7@gmail.com>
2021-01-09 21:44:28 +0800

    optimal solution, could clean up code

--- epi_judge_python/lowest_common_ancestor_with_parent.py
+++ epi_judge_python/lowest_common_ancestor_with_parent.py
@@ -3,15 +3,33 @@ from typing import Optional
 
 from binary_tree_with_parent_prototype import BinaryTreeNode
 from test_framework import generic_test
-from test_framework.binary_tree_utils import must_find_node
+from test_framework.binary_tree_utils import convert_binary_tree_to_binary_tree_with_parent, must_find_node
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
 
 
 def lca(node0: BinaryTreeNode,
         node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
-    # TODO - you fill in here.
-    return None
+    def get_height(node: BinaryTreeNode) -> int:
+        height = 0
+        while node.parent:
+            height += 1
+            node = node.parent
+        return height
+
+    if not node0 or not node1:
+        return None
+
+    node0_height = get_height(node0)
+    node1_height = get_height(node1)
+    longer = node0 if node0_height >= node1_height else node1
+    shorter = node0 if node0_height < node1_height else node1
+    for _ in range(abs(node0_height - node1_height)):
+        longer = longer.parent
+    while shorter is not longer:
+        shorter = shorter.parent
+        longer = longer.parent
+    return shorter
 
 
 @enable_executor_hook
