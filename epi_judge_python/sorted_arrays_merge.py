5288d93dc1a362483354138c36ab173936226259
alexliusq <uilxela7@gmail.com>
2021-01-10 15:11:56 +0800

    should be close to optimal, with fancy named tuple

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -1,11 +1,24 @@
 from typing import List
+import heapq
+from collections import namedtuple
 
 from test_framework import generic_test
 
-
 def merge_sorted_arrays(sorted_arrays: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    ValueAndArrayMeta = namedtuple('ValueAndArrayMeta', ['value', 'array', 'index'])
+    next_heap: List[ValueAndArrayMeta] = []
+    result = []
+    for index, array in enumerate(sorted_arrays):
+        if array:
+            heapq.heappush(next_heap, ValueAndArrayMeta(array[0], index, 0))
+    while next_heap:
+        next = heapq.heappop(next_heap)
+        if next.index < len(sorted_arrays[next.array]) - 1:
+            new_index = next.index + 1
+            heapq.heappush(next_heap, ValueAndArrayMeta(
+                sorted_arrays[next.array][new_index], next.array, new_index))
+        result.append(next.value)
+    return result
 
 
 if __name__ == '__main__':
