c47ec132d239c74164a7a95b4c3a536d6ef6108d
alexliusq <uilxela7@gmail.com>
2021-01-11 11:37:02 +0800

    your solution works but not the shortest

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,9 +4,26 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    def binary_search(a: List[int], target, start, end) -> int:
+        if start < 0 or end > len(a) - 1:
+            return -1
+        while start <= end:
+            middle = start + (end - start) // 2
+            if a[middle] == target:
+                return middle
+            elif a[middle] > target:
+                end = middle - 1
+            else:
+                start = middle + 1
+    
+        return -1
+    
+    prev_hit = next_hit = binary_search(A, k, 0, len(A) - 1)
+    while next_hit > -1:
+        prev_hit = next_hit
+        next_hit = binary_search(A, k, 0, prev_hit - 1)
 
+    return prev_hit
 
 if __name__ == '__main__':
     exit(
