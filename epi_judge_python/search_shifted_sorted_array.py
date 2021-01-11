0dcf726516b85ea38e55bd7dbdb05f3175992ce8
alexliusq <uilxela7@gmail.com>
2021-01-11 14:45:51 +0800

    the far cleaner solution that doesn't branch

--- epi_judge_python/search_shifted_sorted_array.py
+++ epi_judge_python/search_shifted_sorted_array.py
@@ -3,28 +3,38 @@ from typing import List
 from test_framework import generic_test
 
 
+# def search_smallest(A: List[int]) -> int:
+
+#     def smallest(A: List[int], start, end) -> int:
+#         if start == end:
+#             return start
+
+#         if A[start] < A[end]:
+#             return start
+#         else:
+#             middle = (start + end) // 2
+#             left_smallest = smallest(A, start, middle)
+#             right_smallest = smallest(A, middle + 1, end)
+#             if A[left_smallest] <= A[right_smallest]:
+#                 return left_smallest
+#             else:
+#                 return right_smallest
+
+#     start = 0
+#     end = len(A) - 1
+
+#     return smallest(A, start, end)
+
 def search_smallest(A: List[int]) -> int:
-
-    def smallest(A: List[int], start, end) -> int:
-        if start == end:
-            return start
-
-        if A[start] < A[end]:
-            return start
-        else:
-            middle = (start + end) // 2
-            left_smallest = smallest(A, start, middle)
-            right_smallest = smallest(A, middle + 1, end)
-            if A[left_smallest] <= A[right_smallest]:
-                return left_smallest
-            else:
-                return right_smallest
-
     start = 0
     end = len(A) - 1
-
-    return smallest(A, start, end)
-
+    while start < end:
+        middle = (start + end) // 2
+        if A[middle] > A[end]:
+            start = middle + 1
+        elif A[middle] < A[end]:
+            end = middle
+    return start
 
 if __name__ == '__main__':
     exit(
