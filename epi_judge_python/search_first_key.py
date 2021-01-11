803d7e2eaee6771ccdc1d580ff235688b8935e87
alexliusq <uilxela7@gmail.com>
2021-01-11 11:39:38 +0800

    far cleaner way of searching until remainder is exhausted

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,26 +4,21 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    def binary_search(a: List[int], target, start, end) -> int:
-        if start < 0 or end > len(a) - 1:
-            return -1
-        while start <= end:
-            middle = start + (end - start) // 2
-            if a[middle] == target:
-                return middle
-            elif a[middle] > target:
-                end = middle - 1
-            else:
-                start = middle + 1
-    
-        return -1
-    
-    prev_hit = next_hit = binary_search(A, k, 0, len(A) - 1)
-    while next_hit > -1:
-        prev_hit = next_hit
-        next_hit = binary_search(A, k, 0, prev_hit - 1)
 
-    return prev_hit
+    start = 0
+    end = len(A) - 1
+    result = -1
+    while start <= end:
+        middle = start + (end - start) // 2
+        if A[middle] == k:
+            result = middle
+            end = middle - 1
+        elif A[middle] > k:
+            end = middle - 1
+        else:
+            start = middle + 1
+
+    return result
 
 if __name__ == '__main__':
     exit(
