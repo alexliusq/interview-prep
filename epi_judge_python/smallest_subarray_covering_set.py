3927fffaab9d734b7fb9eaf52ff13e1478350852
alexliusq <uilxela7@gmail.com>
2021-01-13 19:49:53 +0800

    your actually more pythonic solution

--- epi_judge_python/smallest_subarray_covering_set.py
+++ epi_judge_python/smallest_subarray_covering_set.py
@@ -8,11 +8,30 @@ from test_framework.test_utils import enable_executor_hook
 
 Subarray = collections.namedtuple('Subarray', ('start', 'end'))
 
+from collections import OrderedDict
 
 def find_smallest_subarray_covering_set(paragraph: List[str],
                                         keywords: Set[str]) -> Subarray:
-    # TODO - you fill in here.
-    return Subarray(0, 0)
+    smallest_cover = Subarray(0,len(paragraph))
+    keyword_locations = OrderedDict()
+    # print('NEW')
+    for index, word in enumerate(paragraph):
+        if word in keywords:
+            if word in keyword_locations:
+                del keyword_locations[word]
+            keyword_locations[word] = index
+            # print(list(keyword_locations.items()))
+        if len(keyword_locations) == len(keywords):
+            locations = list(keyword_locations.values())
+            # print(locations)
+            start = locations[0]
+            end = locations[-1]
+            # print(start, end)
+            current_cover_length = smallest_cover.end - smallest_cover.start
+            if end - start < current_cover_length:
+                smallest_cover = Subarray(start, end)
+    
+    return smallest_cover
 
 
 @enable_executor_hook
