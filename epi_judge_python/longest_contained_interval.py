3b36ea97936029fd58eca370fcd60c37effb4186
alexliusq <uilxela7@gmail.com>
2021-01-13 20:41:55 +0800

    slightly cleaned up

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -5,15 +5,11 @@ from test_framework import generic_test
 
 def longest_contained_range(A: List[int]) -> int:
     candidates = set(A)
-    range_lengths = []
-    for num in A:
-        if not candidates:
-            break
-        if num not in candidates:
-            continue
+    max_range = 0
+    while candidates:
+        num = candidates.pop()
         range_length = 1
         left = right = num
-        candidates.remove(num)
         while (left - 1) in candidates:
             left = left - 1
             range_length += 1
@@ -22,9 +18,9 @@ def longest_contained_range(A: List[int]) -> int:
             right = right + 1
             range_length += 1
             candidates.remove(right)
-        range_lengths.append(range_length)
+        max_range = max(max_range, range_length)
         
-    return max(range_lengths)
+    return max_range
 
 
 if __name__ == '__main__':
