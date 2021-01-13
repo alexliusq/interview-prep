9db19b5e887cbff2631af78e27de868537455324
alexliusq <uilxela7@gmail.com>
2021-01-13 14:20:03 +0800

    epi has more pythonic version but i think this is more approachable'

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,11 +1,27 @@
+from functools import cached_property
 from typing import List
 
 from test_framework import generic_test
 
+from collections import defaultdict
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+    char_locations = defaultdict(list)
+    for index, word in enumerate(paragraph):
+        char_locations[word].append(index)
+
+    char_differences = {}
+    # print(char_locations)
+    for word in char_locations:
+        differences = [end - start for end, start in
+                        zip(char_locations[word][1:], char_locations[word][:-1])]
+        # print(differences)
+        if differences:
+            char_differences[word] = min(differences)
+    # print(char_differences)
+    if char_differences:
+        return min(difference for difference in char_differences.values())
+    return -1
 
 
 if __name__ == '__main__':
