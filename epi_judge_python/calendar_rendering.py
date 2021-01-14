505025068c5dff70bdebf263bdeca7e6bac4bc58
alexliusq <uilxela7@gmail.com>
2021-01-14 20:45:59 +0800

    your solution, which works, but needs a doubly linked list to actually run fast

--- epi_judge_python/calendar_rendering.py
+++ epi_judge_python/calendar_rendering.py
@@ -10,10 +10,27 @@ Event = collections.namedtuple('Event', ('start', 'finish'))
 
 
 def find_max_simultaneous_events(A: List[Event]) -> int:
-    # TODO - you fill in here.
-    return 0
+    events = collections.deque(sorted(A))
+    levels = []
+    while events:
+        next_level = []
+        for event in events:
+            if not next_level:
+                next_level.append(event)
+                continue
 
+            if event.start > next_level[-1].finish:
+                next_level.append(event)
 
+        levels.append(next_level)
+        for event in next_level:
+            events.remove(event)
+        # print(events)
+        # print(len(events))
+    # print('levels')
+    # for level in levels:
+        # print(level)
+    return len(levels)
 @enable_executor_hook
 def find_max_simultaneous_events_wrapper(executor, events):
     events = [Event(*x) for x in events]
