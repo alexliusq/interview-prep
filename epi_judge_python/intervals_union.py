49ff586f94354169f507c938d4ef3c1a69b29963
alexliusq <uilxela7@gmail.com>
2021-01-14 22:46:50 +0800

    your solution is more verbose but logically clearer

--- epi_judge_python/intervals_union.py
+++ epi_judge_python/intervals_union.py
@@ -1,4 +1,5 @@
 import collections
+from decimal import ROUND_05UP
 import functools
 from typing import List
 
@@ -11,8 +12,40 @@ Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 
 def union_of_intervals(intervals: List[Interval]) -> List[Interval]:
-    # TODO - you fill in here.
-    return []
+    def larger_right(interval0: Interval, interval1: Interval) -> Endpoint:
+        if interval0.right.val > interval1.right.val:
+            return interval0.right
+        elif interval1.right.val > interval0.right.val:
+            return interval1.right
+        else: ## interval0.right.val == interval1.right.val
+            if interval1.right.is_closed or interval0.right.is_closed:
+                return Endpoint(True, interval0.right.val)
+            else:
+                return Endpoint(False, interval0.right.val)
+
+    intervals.sort(key =lambda x:
+                   (x.left.val, not x.left.is_closed, x.right, not x.right.is_closed)
+                   )
+    # print([(x.left.val, x.left.is_closed) for x in intervals])
+    if not intervals:
+        return []
+    result = [intervals[0]]
+    iter = 0
+    for iter in range(1, len(intervals)):
+        union = result[-1]
+        curr = intervals[iter]
+        # print('curr', curr)
+        # print('union', union)
+        if curr.left.val < union.right.val:
+            result[-1] = Interval(union.left, larger_right(union, curr))
+        elif (curr.left.val == union.right.val and
+            (curr.left.is_closed or union.right.is_closed)
+            ):
+            result [-1] = Interval(union.left, larger_right(union, curr))
+        else:
+            result.append(curr)
+
+    return result
 
 
 @enable_executor_hook
