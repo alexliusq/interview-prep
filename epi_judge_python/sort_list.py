49d73c806be0d6cf8bb63b7a376023ffc7e319bc
alexliusq <uilxela7@gmail.com>
2021-01-15 00:18:41 +0800

    stable sort merge

--- epi_judge_python/sort_list.py
+++ epi_judge_python/sort_list.py
@@ -44,7 +44,7 @@ def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
     dummy_head = tail = ListNode()
     while L1 and L2:
-        if L1.data < L2.data:
+        if L1.data <= L2.data: ## to make it stable
             tail.next = L1
             L1 = L1.next
         else:
