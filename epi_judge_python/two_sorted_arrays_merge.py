20652025050e703ee4bf858925d9453a97c613fa
alexliusq <uilxela7@gmail.com>
2021-01-14 12:16:41 +0800

    your O(m) solution

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -1,11 +1,28 @@
+from decimal import Inexact
 from typing import List
 
 from test_framework import generic_test
-
+from collections import deque
 
 def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
                             n: int) -> None:
-    # TODO - you fill in here.
+    iter_b = 0
+    temp_storage = deque()
+    for i in range(m):
+        temp_storage.append(A[i])
+    iter_b = 0
+    for index in range(len(A)):
+        if temp_storage and iter_b < n:
+            if temp_storage[0] < B[iter_b]:
+                A[index] = temp_storage.popleft()
+            else:
+                A[index] = B[iter_b]
+                iter_b += 1
+        elif temp_storage:
+            A[index] = temp_storage.popleft()
+        else:
+            A[index] = B[iter_b]
+            iter_b += 1
     return
 
 
