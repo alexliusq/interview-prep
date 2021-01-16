797c91f4153b526297aeac1d5ca0dcb55d3c78ea
alexliusq <uilxela7@gmail.com>
2021-01-16 17:04:23 +0800

    solution

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,8 +4,26 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    def row_placement(row):
+        if row == n:
+            result.append(list(column_placements))
+            return
+        available_cols = set(range(n))
+        for prev_row in range(row):
+            prev_col = column_placements[prev_row]
+            diff = row - prev_row
+            remove_cols = set([prev_col, prev_col - diff, prev_col + diff])
+            available_cols -= remove_cols
+        for col in available_cols:
+            column_placements[row] = col
+            row_placement(row + 1)
+
+
+
+    result = []
+    column_placements = [0] * n
+    row_placement(0)
+    return result
 
 
 def comp(a, b):
