62d155aa1a614c331741fb80aca85f836f38bc0f
alexliusq <uilxela7@gmail.com>
2021-01-16 20:30:59 +0800

    the pretty genius epi solution

--- epi_judge_python/permutations.py
+++ epi_judge_python/permutations.py
@@ -3,10 +3,37 @@ from typing import List
 from test_framework import generic_test, test_utils
 
 
-def permutations(A: List[int]) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+# def permutations(A: List[int]) -> List[List[int]]:
+#     def next_permutation(A: List[int], permutation: List[int], permutation_length):
+#         # print(A)
+#         if not A:
+#             result.append(list(permutation))
+#             return 
+#         for idx, num in enumerate(A):
+#             permutation[permutation_length] = num
+#             shrunk_a = A[:idx] + A[idx + 1:]
+#             next_permutation(shrunk_a, permutation, permutation_length + 1)
 
+#     result = []
+#     permutation = [0] * len(A)
+#     next_permutation(A, permutation, 0)
+#     return result
+
+## the cleaner, O(1) space solution
+def permutations(A: List[int]) -> List[List[int]]:
+    def next_permutation(index):
+        if index == len(A) - 1:
+            result.append(A.copy())
+            return
+        for next_start in range(index, len(A)):
+            A[next_start], A[index] = A[index], A[next_start]
+            next_permutation(index + 1)
+            ## swap it back after it's all done. pretty genius
+            A[index], A[next_start] = A[next_start], A[index]
+
+    result = []
+    next_permutation(0)
+    return result
 
 if __name__ == '__main__':
     exit(
