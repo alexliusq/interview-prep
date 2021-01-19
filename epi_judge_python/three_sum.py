26f6bdf66ee8bd8f8b0f136060f6d68fcb1378b8
alexliusq <uilxela7@gmail.com>
2021-01-19 20:20:15 +0800

    wow ok reducing space complexity does speed it tup a lot

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -3,17 +3,38 @@ from typing import List
 from test_framework import generic_test
 
 
+# def has_three_sum(A: List[int], t: int) -> bool:
+#     # TODO - you fill in here.
+#     pair_dict = {}
+#     for i in range(len(A)):
+#         for j in range(i ,len(A)):
+#             sum = A[i] + A[j]
+#             pair_dict[sum] = [A[i], A[j]]
+#     for num in A:
+#         if pair_dict.get(t - num):
+#             return True
+#     return False
+
+def has_two_sum(A: List[int], target_sum: int) -> bool:
+    i = 0
+    j = len(A) - 1
+    while i <= j:
+        pair_sum = A[i] + A[j]
+        if pair_sum == target_sum:
+            return True
+        elif pair_sum > target_sum:
+            j -= 1
+        else: # pair sum < target_sum
+            i += 1
+
 def has_three_sum(A: List[int], t: int) -> bool:
     # TODO - you fill in here.
-    pair_dict = {}
-    for i in range(len(A)):
-        for j in range(i ,len(A)):
-            sum = A[i] + A[j]
-            pair_dict[sum] = [A[i], A[j]]
+    A.sort()
     for num in A:
-        if pair_dict.get(t - num):
+        if has_two_sum(A, t - num):
             return True
     return False
+
 
 
 if __name__ == '__main__':
