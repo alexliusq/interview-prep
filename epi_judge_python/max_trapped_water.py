7fd9e28d0e50342c6763181aac94a493fd61f6a5
alexliusq <uilxela7@gmail.com>
2021-01-20 14:31:58 +0800

    solution that isn't as clean as epi but captures the core idea

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -4,8 +4,23 @@ from test_framework import generic_test
 
 
 def get_max_trapped_water(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    left = max_left = 0
+    right = max_right = len(heights) - 1
+    max_water = min(heights[max_left], heights[max_right]) * (max_right - max_left)
+
+    while right > left:
+        if heights[right] <= heights[left]:
+            right -= 1
+        else:
+            left += 1
+        
+        new_max = min(heights[left], heights[right]) * (right - left)
+        if new_max > max_water:
+            max_water = new_max
+            max_left = left
+            max_right = right
+    # print(max_left, max_right)
+    return max_water
 
 
 if __name__ == '__main__':
