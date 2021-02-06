b83b3f364db4e5debcbc9b0b4d8088d8d67a4611
alexliusq <uilxela7@gmail.com>
2021-02-06 17:47:38 +0800

    solution to valid bst

---
+++ leetcode/validate_binary_serach_tree.py
@@ -0,0 +1,32 @@
+
+def isValidBST(root: TreeNode) -> bool:
+    def bst_helper(root: TreeNode):
+        if not root:
+            return (True, (0,0))
+        
+        current_range = [root.val, root.val]
+        
+        if root.left:
+            if not (root.left.val < root.val):
+                return (False, (0,0))
+            left_is_valid, left_range = bst_helper(root.left)
+            if not left_is_valid:
+                return (False, (0,0))
+            if not (left_range[1] < root.val):
+                return (False, (0,0))
+            current_range[0] = left_range[0]
+        
+        if root.right:
+            if not (root.right.val > root.val):
+                return (False, (0,0))
+            right_is_valid, right_range = bst_helper(root.right)
+            if not right_is_valid:
+                return (False, (0,0))
+            if not (right_range[0] > root.val):
+                return (False, (0,0))
+            current_range[1] = right_range[1]
+        
+        return (True, tuple(current_range))
+    
+    valid_bst, _ = bst_helper(root)
+    return valid_bst
