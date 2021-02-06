6e1a664392fbc165630d445326903a0be1264e8f
alexliusq <uilxela7@gmail.com>
2021-02-06 20:01:05 +0800

    working solution

--- leetcode/generate_parentheses.py
+++ leetcode/generate_parentheses.py
@@ -1,22 +1,37 @@
 from typing import List
 
-def generateParenthesis(n: int) -> List[str]:
-    def next_set(left, right, n):
-        print(n)
-        if n <= 0:
-            result.add(left + right)
+# def generateParenthesis(n: int) -> List[str]:
+#     def next_set(left, right, n):
+#         print(n)
+#         if n <= 0:
+#             result.add(left + right)
+#             return
+        
+#         next_set('()' + left, right, n - 1)
+#         next_set('(' + left + ')', right, n - 1)
+#         next_set(left + '(', ')' + right, n - 1)
+#         next_set(left, '(' + right + ')', n - 1)
+#         next_set(left, right + '()', n - 1)
+#         next_set('(' + left, right + ')', n - 1)
+        
+#     result = set()
+#     next_set('', '', n)
+#     return list(result)
+
+def generateParenthesis(self, n: int) -> List[str]:
+    def backtrack(parens, left, right):
+        if left == n and right == n:
+            result.append(parens)
             return
         
-        next_set('()' + left, right, n - 1)
-        next_set('(' + left + ')', right, n - 1)
-        next_set(left + '(', ')' + right, n - 1)
-        next_set(left, '(' + right + ')', n - 1)
-        next_set(left, right + '()', n - 1)
-            
-        
-    result = set()
-    next_set('', '', n)
-    return list(result)
+        if left < n:
+            backtrack(parens + '(', left + 1, right)
+        if right < left:
+            backtrack(parens + ')', left, right + 1)
+    
+    result = []
+    backtrack('', 0, 0)
+    return result
     
 
 print(generateParenthesis(4))
