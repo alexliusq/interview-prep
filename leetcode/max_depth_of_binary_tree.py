4e88ae88f9d4f0d205f79ff8eae86e78345e167a
alexliusq <uilxela7@gmail.com>
2021-02-06 18:11:15 +0800

    more elegant solution

--- leetcode/max_depth_of_binary_tree.py
+++ leetcode/max_depth_of_binary_tree.py
@@ -5,3 +5,9 @@ def maxDepth(self, root: TreeNode) -> int:
         return max(depth_helper(root.left, depth + 1), depth_helper(root.right, depth + 1))
     
     return depth_helper(root, 0)
+
+def maxDepth(self, root: TreeNode) -> int:
+    if not root:
+        return 0
+        
+    return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
