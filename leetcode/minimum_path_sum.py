c223c1fb98808edf81939ac58247b7a49e31eb85
alexliusq <uilxela7@gmail.com>
2021-02-08 00:28:25 +0800

    bottom up solution

--- leetcode/minimum_path_sum.py
+++ leetcode/minimum_path_sum.py
@@ -13,3 +13,18 @@ class Solution:
                 return min_path(row - 1, col) + grid[row][col]
         
         return min_path(len(grid) - 1, len(grid[0]) - 1)
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if row == 0 and col == 0:
+                    continue
+                elif row == 0 and col > 0:
+                    grid[row][col] += grid[row][col - 1]
+                elif col == 0 and row > 0:
+                    grid[row][col] += grid[row - 1][col]
+                else: ## row > 0 and col > 0
+                    grid[row][col] += min(grid[row - 1][col], grid[row][col - 1])
+        # print(grid)
+        return grid[-1][-1]
