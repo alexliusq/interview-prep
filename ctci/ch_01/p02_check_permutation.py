ddd0e555b17bc07396cca4d6efffd7ef058142d6
alexliusq <uilxela7@gmail.com>
2021-02-14 23:21:01 +0800

    check_permutation solution

---
+++ ctci/ch_01/p02_check_permutation.py
@@ -0,0 +1,28 @@
+import random
+from collections import Counter
+
+##O(n) space use Counter, O(n) time
+## O(nlogn) time, O(1) space, sort and linearly scan
+
+def is_permutation(s1: str, s2: str) -> bool:
+    if len(s1) != len(s2):
+        return False
+    
+    return Counter(s1) == Counter(s2)
+
+sample_strings = [
+    'aabb',
+    'aabbc'
+    'a',
+    'abc'
+    '',
+]
+
+def test_true():
+    for val in sample_strings:
+        shuffled = list(val)
+        random.shuffle(shuffled)
+        shuffled = ''.join(shuffled)
+        assert is_permutation(val, shuffled) == True, f"Failed for val: {val}"
+
+test_true()
