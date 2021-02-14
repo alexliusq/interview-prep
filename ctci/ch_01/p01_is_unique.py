305d8f451ac85b02d38198da6309a1c71335377e
alexliusq <uilxela7@gmail.com>
2021-02-14 22:54:06 +0800

    solution for is_unique

---
+++ ctci/ch_01/p01_is_unique.py
@@ -0,0 +1,38 @@
+import unittest
+from collections import Counter
+
+def is_unique(s: str) -> bool:
+    if len(s) >= 128:
+        return False
+    
+    # return all(count <= 1 for count in Counter(s).values())
+    return False
+
+test_cases = [
+    ('11123', False),
+    ('abdd', False),
+    ('abcd', True),
+    ('1234', True)
+]
+
+for case in test_cases:
+    input_val, expected = case
+    assert is_unique(input_val) == expected, f"FAILED for {input_val}"
+
+# class TestUnique(unittest.TestCase):
+
+#     def test_unique(self):
+#         test_cases = [
+#             ('11123', False),
+#             ('abdd', False),
+#             ('abcd', True),
+#             ('1234', True)
+#         ]
+#         for case in test_cases:
+#             test_string, expected = case
+#             self.assertEqual(is_unique(test_string), expected,
+#                 msg=f"Failed for: {test_string}")
+        
+
+# if __name__ == "__main__":
+#     unittest.main(verbosity=2)
